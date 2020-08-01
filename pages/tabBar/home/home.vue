1088d752a8243063047c2ad431299c48a884412e
laofuzi <41654026@qq.com>
2020-08-01 21:33:49 +0800

    first commit

---
+++ pages/tabBar/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+	<app-header/>
+</template>
+
+<script>
+	import header from "./header.vue"
+	export default {
+		components:{
+			'app-header':header
+		}
+	}
+</script>
+
+<style>
+</style>
---
+++ utils/network.js
@@ -0,0 +1,6 @@
+const baseUrl='https://uniapp-interface.herokuapp.com/'
+
+const interface={
+	homeData:baseUrl+'api/profiles/mall_list'
+}
+module.exports= interface
